ccd725aac5b2ea56a39d08a24b21658e518a484e
Jared Hinze <Jared.L.Hinze@gmail.com>
2025-06-19 00:30:09 -0500

    Create parser package entry point
    All parsers will come from this package and will need to load YAML at some point so I made a generic function to help with that.

---
+++ src/parsers/__init__.py
@@ -0,0 +1,32 @@
+# Python 3.13
+# ==============================================================================
+# Built-in Libraries
+import logging
+
+# Third Party Libraries
+# N/A
+
+# Local Libraries
+from config import yaml
+
+# ==============================================================================
+# Initializers
+# ==============================================================================
+logger = logging.getLogger(__name__)
+
+# ==============================================================================
+# Shared Helpers
+# ==============================================================================
+def load_yaml(file) -> dict | None:
+	data = None
+
+	try:
+		with file.open() as f:
+			data = yaml.load(f)
+
+			if logger.isEnabledFor(logging.DEBUG):
+				logger.debug(data)
+	except Exception as e:
+		logger.exception(e)
+
+	return data
